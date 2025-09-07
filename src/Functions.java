adfee72dee6acdce600765324b5c27257cb71db4
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-09-07 07:12:44 +0600

    solve HollowButterfly pattern

---
+++ src/Functions.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Functions {
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+        printName(name);
+    }
+}
---
+++ src/Patterns/HollowButterfly.java
@@ -0,0 +1,64 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowButterfly {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
